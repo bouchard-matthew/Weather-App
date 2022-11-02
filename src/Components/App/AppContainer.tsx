77eec949f4cb0991e2d86b2d7c0be4f6c57453c7
mabouc4699 <matthewbouchard72@outlook.com>
2022-11-02 19:56:59 -0400

    Added state values

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -5,32 +5,32 @@ import App from "./App";
 const { REACT_APP_API_KEY } = process.env;
 
 const AppContainer = () => {
-  const { lat, long, setLat, setLong } = useStore();
+  const { lat, long, setLat, setLong, units } = useStore();
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      function (position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      },
-      function (error) {
-        console.log(error);
-      },
-      {
-        maximumAge: 60000,
-        timeout: 5000,
-        enableHighAccuracy: true,
-      }
-    );
-
-    if (lat != undefined && long != undefined) {
+    if (lat !== undefined && long !== undefined) {
       axios
         .get(
-          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&appid=${REACT_APP_API_KEY}&units=imperial`
+          `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&appid=${REACT_APP_API_KEY}&units=${units}`
         )
         .then((response) => console.dir(response.data));
+    } else {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          setLat(position.coords.latitude);
+          setLong(position.coords.longitude);
+        },
+        function (error) {
+          console.log(error);
+        },
+        {
+          maximumAge: 60000,
+          timeout: 5000,
+          enableHighAccuracy: true,
+        }
+      );
     }
-  }, [lat, long]);
+  }, [lat, long, units]);
 
   return <App />;
 };
--- src/Context/useAppStore.ts
+++ src/Context/useAppStore.ts
@@ -1,21 +1,27 @@
 import create from "zustand";
+import { Units } from "../Types/types";
 
 export type StoreState = {
   lat: Number | undefined;
   long: Number | undefined;
   setLat: (latitude: Number) => void;
   setLong: (longitude: Number) => void;
+  units: Units;
 };
 
 export const useStore = create<StoreState>((set) => ({
   // initial state
   lat: undefined,
   long: undefined,
+  units: Units.imperial,
   // methods for manipulating state
   setLat: (latitude: Number) => {
     set(() => ({ lat: latitude }));
   },
   setLong: (longitude: Number) => {
     set(() => ({ long: longitude }));
+  },
+  setUnits: (unit: Units) => {
+    set(() => ({ units: unit }));
   },
 }));
