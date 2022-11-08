693b63690fe5cb72fb495f02fd30f7c09d951324
mabouc4699 <matthewbouchard72@outlook.com>
2022-11-07 19:31:43 -0500

    Adding routes and placeholder components

---
+++ src/Components/Current/CurrentContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Current from "./Current";
+
+const CurrentContainer = () => {
+  return <Current />;
+};
+
+export default CurrentContainer;
---
+++ src/Components/Hourly/index.tsx
@@ -0,0 +1,1 @@
+export { default as Hourly } from "./HourlyContainer";
