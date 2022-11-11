fba9d92baad40c595c035d62ac03efa212b0ea3b
bouchard-matthew <matthewbouchard72@outlook.com>
2022-11-10 20:07:11 -0500

    Header rough draft

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,9 +5,31 @@ require("./Header.css");
 const Header = () => {
   return (
     <>
-    <div className={"weather"}>
-      something
-    </div>
+      <div className={"main"}>
+        <div>
+          <p>Weather</p>
+        </div>
+        <div>
+          Search Icon <input />
+        </div>
+        <div>
+          <p>Option menu</p>
+        </div>
+      </div>
+      <div className={"weather"}>
+        <div>
+          <img alt="img1" src="http://openweathermap.org/img/wn/10d.png" />
+          <span>Rainy</span>
+        </div>
+        <div>
+          <img alt="img2" src="http://openweathermap.org/img/wn/02d.png" />
+          <span>Partly Cloudy</span>
+        </div>
+        <div>
+          <img alt="img3" src="http://openweathermap.org/img/wn/01d.png" />
+          <span>Sunny</span>
+        </div>
+      </div>
       <nav className={"navigation"}>
         <ul className={"listItems"}>
           <li>
