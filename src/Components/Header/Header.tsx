22d331a308a00e81a4ba847a809a9490206ee38f
Andrew Chanthavisith <61341183+Achanthavisith@users.noreply.github.com>
2022-11-10 19:46:31 -0500

    initial header commit

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,45 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+require("./Header.css");
 
 const Header = () => {
-  let activeStyle = {
-    textDecoration: "underline",
-  };
-
-  let activeClassName = "underline";
-
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink
-            to="/"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            Today
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="weekly"
-            className={({ isActive }) =>
-              isActive ? activeClassName : undefined
-            }
-          >
-            Weekly
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="hourly">
-            {({ isActive }) => (
-              <span className={isActive ? activeClassName : undefined}>
-                Hourly
-              </span>
-            )}
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+    <div className={"weather"}>
+      something
+    </div>
+      <nav className={"navigation"}>
+        <ul className={"listItems"}>
+          <li>
+            <NavLink to="/">Today</NavLink>
+          </li>
+          <li>
+            <NavLink to="weekly">Weekly</NavLink>
+          </li>
+          <li>
+            <NavLink to="hourly">Hourly</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
