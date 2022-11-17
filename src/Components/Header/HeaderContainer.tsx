d777e1cf16b4cb873c31925f56be71fc5dee603b
Andrew Chanthavisith <61341183+Achanthavisith@users.noreply.github.com>
2022-11-17 16:10:50 -0500

    push

--- src/Components/Header/HeaderContainer.tsx
+++ src/Components/Header/HeaderContainer.tsx
@@ -2,7 +2,21 @@ import * as React from "react";
 import Header from "./Header";
 
 const HeaderContainer = () => {
-  return <Header />;
+  const [zip, setZip] = React.useState('');
+  const inputHandler = (e: any) => {
+// setZip(e.target.value);
+const elem = document.querySelector('[aria-label="search"]');
+elem?.setAttribute('value', e.target.value);
+console.log(e.target.value);
+console.log(elem);
+  }
+
+  const setValue = () => {
+    const elem = document.querySelector('[aria-label="search"]');
+// setZip(elem.value == null ? "" : elem.value);
+console.group(elem);
+  }
+  return <Header handleState={setValue} handler={inputHandler}/>;
 };
 
 export default HeaderContainer;
