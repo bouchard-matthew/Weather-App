ef5adbed742fd185e55dc9f2209d5054f3a4ea2e
bouchard-matthew <matthewbouchard72@outlook.com>
2022-11-18 10:25:32 -0500

    Removing unnecessary imports

--- src/Components/Header/HeaderContainer.tsx
+++ src/Components/Header/HeaderContainer.tsx
@@ -1,12 +1,13 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const HeaderContainer = () => {
-const [zip, setZip] = React.useState('');
+  const [zip, setZip] = useState("");
 
-const setValue = (e: any) => {
-  setZip(e.target.value);
-  console.log(zip);
-}
+  const setValue = (e: any) => {
+    setZip(e.target.value);
+    console.log(zip);
+  };
   return <Header handleState={setValue} />;
 };
 
