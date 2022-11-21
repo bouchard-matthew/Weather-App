5c63004611bfe58e78296d329cfbe466742231ae
bouchard-matthew <matthewbouchard72@outlook.com>
2022-11-20 20:34:53 -0500

    Roughly finished hourly components

--- src/Utils/dataFunctions.ts
+++ src/Utils/dataFunctions.ts
@@ -9,6 +9,7 @@ export const prepareHourlyForRendering = (hourlyData: Current[]): NewHourlyObjec
       "Feels Like": Math.round(item.feels_like),
       Clouds: Math.round(item.clouds),
       Temperature: Math.round(item.temp),
+      Precipitation: Math.round(item.pop),
     });
     array.push(obj);
   });
@@ -68,6 +69,42 @@ export const setLatAndLong = (setLat: (latitude: Number) => void, setLong: (long
 export const userWeatherDataAvailable = () => {
   let userWeather = localStorage.getItem("weather");
   return userWeather === null;
+};
+
+export const degToCard = (deg: number) => {
+  if (deg > 11.25 && deg <= 33.75) {
+    return "NNE";
+  } else if (deg > 33.75 && deg <= 56.25) {
+    return "ENE";
+  } else if (deg > 56.25 && deg <= 78.75) {
+    return "E";
+  } else if (deg > 78.75 && deg <= 101.25) {
+    return "ESE";
+  } else if (deg > 101.25 && deg <= 123.75) {
+    return "ESE";
+  } else if (deg > 123.75 && deg <= 146.25) {
+    return "SE";
+  } else if (deg > 146.25 && deg <= 168.75) {
+    return "SSE";
+  } else if (deg > 168.75 && deg <= 191.25) {
+    return "S";
+  } else if (deg > 191.25 && deg <= 213.75) {
+    return "SSW";
+  } else if (deg > 213.75 && deg <= 236.25) {
+    return "SW";
+  } else if (deg > 236.25 && deg <= 258.75) {
+    return "WSW";
+  } else if (deg > 258.75 && deg <= 281.25) {
+    return "W";
+  } else if (deg > 281.25 && deg <= 303.75) {
+    return "WNW";
+  } else if (deg > 303.75 && deg <= 326.25) {
+    return "NW";
+  } else if (deg > 326.25 && deg <= 348.75) {
+    return "NNW";
+  } else {
+    return "N";
+  }
 };
 
 /* prepDataForWeatherArray accepts the following parameters:
