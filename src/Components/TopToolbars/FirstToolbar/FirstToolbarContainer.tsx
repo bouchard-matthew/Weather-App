01df499b3102e118f7f3511bed55f53feee006c8
Andrew Chanthavisith <61341183+Achanthavisith@users.noreply.github.com>
2022-11-21 10:49:55 -0500

    useEffect for toolbar

--- src/Components/TopToolbars/FirstToolbar/FirstToolbarContainer.tsx
+++ src/Components/TopToolbars/FirstToolbar/FirstToolbarContainer.tsx
@@ -1,11 +1,14 @@
 import FirstToolbar from "./FirstToolbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useStore } from "Context/useAppStore";
+import axios from "axios";
+
+const { REACT_APP_API_KEY } = process.env;
 
 let defaultVal: string = "";
 
 const FirstToolbarContainer = () => {
-  const { units, setUnits } = useStore();
+  const { lat, long, setLong ,setLat, setUnits, units } = useStore();
   const [zip, setZip] = useState<string>(defaultVal);
 
   const setValue = () => {
@@ -15,6 +18,23 @@ const FirstToolbarContainer = () => {
   const setElementValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     defaultVal = e.target.value;
   };
+
+  const fetchWeatherDataZip = async() => {
+    let res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${REACT_APP_API_KEY}`
+    )
+
+    setLat(res.data.coord.lat);
+    setLong(res.data.coord.lon);
+}
+
+
+useEffect(() => {
+    if (zip !== '') {
+        fetchWeatherDataZip();
+    }
+}, [zip])
+
 
   return <FirstToolbar handleState={setValue} handler={setElementValue} units={units} setUnits={setUnits} />;
 };
