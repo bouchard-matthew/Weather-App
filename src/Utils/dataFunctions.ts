aa9de9a0cc2190a5b87e4ffff72c9fece83c018f
bouchard-matthew <matthewbouchard72@outlook.com>
2022-11-20 20:40:22 -0500

    Converting function in dataFunctions to switch

--- src/Utils/dataFunctions.ts
+++ src/Utils/dataFunctions.ts
@@ -72,38 +72,39 @@ export const userWeatherDataAvailable = () => {
 };
 
 export const degToCard = (deg: number) => {
-  if (deg > 11.25 && deg <= 33.75) {
-    return "NNE";
-  } else if (deg > 33.75 && deg <= 56.25) {
-    return "ENE";
-  } else if (deg > 56.25 && deg <= 78.75) {
-    return "E";
-  } else if (deg > 78.75 && deg <= 101.25) {
-    return "ESE";
-  } else if (deg > 101.25 && deg <= 123.75) {
-    return "ESE";
-  } else if (deg > 123.75 && deg <= 146.25) {
-    return "SE";
-  } else if (deg > 146.25 && deg <= 168.75) {
-    return "SSE";
-  } else if (deg > 168.75 && deg <= 191.25) {
-    return "S";
-  } else if (deg > 191.25 && deg <= 213.75) {
-    return "SSW";
-  } else if (deg > 213.75 && deg <= 236.25) {
-    return "SW";
-  } else if (deg > 236.25 && deg <= 258.75) {
-    return "WSW";
-  } else if (deg > 258.75 && deg <= 281.25) {
-    return "W";
-  } else if (deg > 281.25 && deg <= 303.75) {
-    return "WNW";
-  } else if (deg > 303.75 && deg <= 326.25) {
-    return "NW";
-  } else if (deg > 326.25 && deg <= 348.75) {
-    return "NNW";
-  } else {
-    return "N";
+  switch (true) {
+    case deg > 11.25 && deg <= 33.75:
+      return "NNE";
+    case deg > 33.75 && deg <= 56.25:
+      return "ENE";
+    case deg > 56.25 && deg <= 78.75:
+      return "E";
+    case deg > 78.75 && deg <= 101.25:
+      return "ESE";
+    case deg > 101.25 && deg <= 123.75:
+      return "ESE";
+    case deg > 123.75 && deg <= 146.25:
+      return "SE";
+    case deg > 146.25 && deg <= 168.75:
+      return "SSE";
+    case deg > 168.75 && deg <= 191.25:
+      return "S";
+    case deg > 191.25 && deg <= 213.75:
+      return "SSW";
+    case deg > 213.75 && deg <= 236.25:
+      return "SW";
+    case deg > 236.25 && deg <= 258.75:
+      return "WSW";
+    case deg > 258.75 && deg <= 281.25:
+      return "W";
+    case deg > 281.25 && deg <= 303.75:
+      return "WNW";
+    case deg > 303.75 && deg <= 326.25:
+      return "NW";
+    case deg > 326.25 && deg <= 348.75:
+      return "NNW";
+    default:
+      return "N";
   }
 };
 
