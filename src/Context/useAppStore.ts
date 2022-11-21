772de7979df6960ed6f5c6910b049b59758710c2
bouchard-matthew <matthewbouchard72@outlook.com>
2022-11-21 08:47:50 -0500

    Morning commit - cleaning up

--- src/Context/useAppStore.ts
+++ src/Context/useAppStore.ts
@@ -2,18 +2,11 @@ import create from "zustand";
 import { Units, Current, Weather } from "Types/types";
 import { prepDataForWeatherArray } from "Utils/dataFunctions";
 
-type UserLocation = {
-  lat: Number | undefined;
-  long: Number | undefined;
-};
-
 export type StoreState = {
   lat: Number | undefined;
   long: Number | undefined;
-  location: UserLocation;
   setLat: (latitude: Number) => void;
   setLong: (longitude: Number) => void;
-  setLocation: (data: UserLocation) => void;
   units: Units;
   hourly: Current[];
   weather: Weather | undefined;
@@ -27,10 +20,6 @@ export const useStore = create<StoreState>((set) => ({
   long: undefined,
   units: Units.imperial,
   hourly: [],
-  location: {
-    lat: undefined,
-    long: undefined,
-  },
   weather: undefined,
   weatherArray: [],
   // methods for manipulating state
@@ -42,14 +31,6 @@ export const useStore = create<StoreState>((set) => ({
   },
   setUnits: (unit: Units) => {
     set(() => ({ units: unit }));
-  },
-  setLocation: (data: UserLocation) => {
-    set(() => ({
-      location: {
-        lat: data.lat,
-        long: data.long,
-      },
-    }));
   },
   setWeather: (data: Weather) => {
     // change state of weatherArray when weather is set (return of prepDataForWeatherArray function)
