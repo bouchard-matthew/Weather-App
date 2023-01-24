73d9437cc62781671957899716f1a5499b8844d2
bouchard-matthew <matthewbouchard72@outlook.com>
2023-01-23 19:36:26 -0500

    Rough draft of current component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -55,7 +55,7 @@ const AppContainer = () => {
             }
           />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       </BrowserRouter>
     </>
   );
--- src/Components/Current/Current.tsx
+++ src/Components/Current/Current.tsx
@@ -1,21 +1,110 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { Props } from "./Current.types";
 import dayjs from "dayjs";
 import { capitalizeFirstLetter } from "Utils/stringFunctions";
-import { returnUnitTemperature } from "Utils/dataFunctions";
+import { returnCardinality, returnUnitSpeed, returnUnitTemperature } from "Utils/dataFunctions";
+import { Flex, Paragraph } from "Design";
+import { useHourlyWeather } from "Hooks/useHourlyWeather";
+import Button from "@mui/material/Button";
 
-const Current = ({ current, name, units }: Props) => {
+const Current = ({ current, units, today, toggle, setToggle }: Props) => {
+  let [first, second] = useHourlyWeather().filter(
+    (item) => "12:00 AM" == dayjs(item.dt * 1000).format("hh:mm A") || "12:00 PM" == dayjs(item.dt * 1000).format("hh:mm A")
+  );
+  console.log("First: ", first);
+  console.log("Second: ", second);
   return (
     <>
       {current && (
-        <Box sx={{ textAlign: "center" }}>
-          <Typography>
-            <b>{name}</b> As of {dayjs(current.dt * 1000).format("hh:mm A")}
-          </Typography>
-          <Typography variant="h3">{returnUnitTemperature(current.temp, units)}</Typography>
-          <Typography variant="h4">{capitalizeFirstLetter(current.weather[0].description)}</Typography>
-        </Box>
+        <>
+          <Flex flexDirection={"column"} textAlign={"center"} sx={{ margin: "auto", width: "65%", borderRadius: "5px", border: "1px solid black" }}>
+            <Paragraph>Current Weather</Paragraph>
+            <Paragraph>{dayjs(current.dt * 1000).format("hh:mm A")}</Paragraph>
+            <Flex justifyContent={"center"}>
+              <img src={`http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`} alt="" />
+              <Box>
+                <Paragraph variant="h4">{returnUnitTemperature(current.temp, units)}</Paragraph>
+                <Paragraph>{capitalizeFirstLetter(current.weather[0].description)}</Paragraph>
+              </Box>
+            </Flex>
+            {!toggle ? (
+              <Button sx={{ margin: "auto", width: "fit-content", paddingX: 10 }} onClick={() => setToggle(!toggle)}>
+                See More
+              </Button>
+            ) : (
+              <>
+                <Box>
+                  <Flex justifyContent={"space-around"}>
+                    <Paragraph>Day</Paragraph>
+                    <Paragraph>{dayjs(current.dt * 1000).format("M/D")}</Paragraph>
+                  </Flex>
+                  <Flex justifyContent={"center"}>
+                    <img src={`http://openweathermap.org/img/wn/${first.weather[0].icon}@2x.png`} alt="" />
+                    <Box>
+                      <Paragraph variant="h4">{returnUnitTemperature(first.temp, units)}</Paragraph>
+                      <Paragraph>{capitalizeFirstLetter(first.weather[0].description)}</Paragraph>
+                    </Box>
+                  </Flex>
+                  <Box>
+                    <Flex flexDirection={"column"}>
+                      <Paragraph>
+                        <b>UV Index</b>
+                        {Math.round(first.uvi)}
+                      </Paragraph>
+                      <Paragraph>
+                        <b>Wind</b>
+                        {returnCardinality(first.wind_deg) + " " + returnUnitSpeed(first.wind_speed, units)}
+                      </Paragraph>
+                      <Paragraph>
+                        <b>Wind Gusts</b>
+                        {returnUnitSpeed(first.wind_gust, units)}
+                      </Paragraph>
+                      <Paragraph>
+                        <b>Probability of Precipitation</b>
+                      </Paragraph>
+                    </Flex>
+                    <Flex flexDirection={"column"}></Flex>
+                  </Box>
+                </Box>
+                <Box>
+                  <Flex justifyContent={"space-around"}>
+                    <Paragraph>Night</Paragraph>
+                    <Paragraph>{dayjs(current.dt * 1000).format("M/D")}</Paragraph>
+                  </Flex>
+                </Box>
+                <Flex justifyContent={"center"}>
+                  <img src={`http://openweathermap.org/img/wn/${second.weather[0].icon}@2x.png`} alt="" />
+                  <Box>
+                    <Paragraph variant="h4">{returnUnitTemperature(second.temp, units)}</Paragraph>
+                    <Paragraph>{capitalizeFirstLetter(second.weather[0].description)}</Paragraph>
+                  </Box>
+                </Flex>
+                <Box>
+                  <Flex flexDirection={"column"}>
+                    <Paragraph>
+                      <b>UV Index</b>
+                      {Math.round(second.uvi)}
+                    </Paragraph>
+                    <Paragraph>
+                      <b>Wind</b>
+                      {returnCardinality(second.wind_deg) + " " + returnUnitSpeed(second.wind_speed, units)}
+                    </Paragraph>
+                    <Paragraph>
+                      <b>Wind Gusts</b>
+                      {returnUnitSpeed(second.wind_gust, units)}
+                    </Paragraph>
+                    <Paragraph>
+                      <b>Probability of Precipitation</b>
+                    </Paragraph>
+                  </Flex>
+                  <Flex flexDirection={"column"}></Flex>
+                </Box>
+                <Button onClick={() => setToggle(!toggle)}>See Less</Button>
+              </>
+            )}
+          </Flex>
+        </>
       )}
     </>
   );
